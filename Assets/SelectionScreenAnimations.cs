22bf45c9d907a84006a5dfb6f623881064509c12
Ademola Afolabi <kingsminos@gmail.com>
2016-02-18 19:51:07 +0000

    Removed code for Selection Screen Animations and consilidated it wih the SelectionScreenComponent script

--- Assets/SelectionScreenAnimations.cs
+++ Assets/SelectionScreenAnimations.cs
@@ -3,35 +3,4 @@ using System.Collections;
 
 public class SelectionScreenAnimations : MonoBehaviour {
 
-    public AnimationClip ContinueButtonFadeIn;
-    public AnimationClip ContinueButtonFadeOut;
-    public AnimationClip ReturnToMenuButtonFadeIn;
-    public AnimationClip ReturnToMenuButtonFadeOut;
-    public AnimationClip ExitButtonFadeIn;
-    public AnimationClip ExitButtonFadeOut;
-
-
-
-	// Use this for initialization
-	void Start () 
-	{
-        /*
-		AnimationEvent ContinueButtonFadeOut_AnimationEvent = new AnimationEvent();
-		ContinueButtonFadeOut_AnimationEvent.functionName = "Return To Game";
-		ContinueButtonFadeOut.AddEvent (ContinueButtonFadeOut_AnimationEvent);
-
-		AnimationEvent ReturnToMenuButtonFadeOut_AnimationEvent = new AnimationEvent ();
-		ReturnToMenuButtonFadeOut_AnimationEvent.functionName = "Return To Menu Screen";
-		ReturnToMenuButtonFadeOut.AddEvent (ReturnToMenuButtonFadeOut_AnimationEvent);
-
-		AnimationEvent ExitButtonFadeOut_AnimationEvent = new AnimationEvent ();
-		ExitButtonFadeOut_AnimationEvent.functionName = "Exit Game";
-		ExitButtonFadeOut.AddEvent (ExitButtonFadeOut_AnimationEvent);
-	*/
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
