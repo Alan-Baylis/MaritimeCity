25de46d103a4604433b772824d589d1e8add9c2c
Ademola Afolabi <kingsminos@gmail.com>
2016-02-19 17:43:11 +0000

    Editor changes #2

---
+++ Assets/StandardAssets/Scripts/Model/Controller/UXController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UXController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
