6c97a0682ff55ae4d058352cf91d55fe9cb79739
Ademola Afolabi <kingsminos@gmail.com>
2016-03-09 15:43:59 +0000

    Building the walk infrastructrue

--- Assets/Standard Assets/Scripts/Model/Controller/BrendanController/BrendanBaseAnimator.cs
+++ Assets/Standard Assets/Scripts/Model/Controller/BrendanController/BrendanBaseAnimator.cs
@@ -3,13 +3,66 @@ using System.Collections;
 
 public class BrendanBaseAnimator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public enum WalkID {
+        active,
+        inactive };
+
+    public static WalkID Walk = WalkID.inactive;
+    private Animator testAnimator;
+
+
+    // Use this for initialization
+    void Start() {
+
+        testAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        InitializeWalkerID();
+   
+    }
+
+    public static void SetWalkIDInactive()
+    {
+        Walk = WalkID.inactive;
+    }
+
+    public static void SetWalkIDActive()
+    {
+        Walk = WalkID.active;
+    }
+
+
+    public void InitializeWalkerID()
+    {
+        switch (Walk)
+        {
+
+            case WalkID.active:
+                if (gameObject != null)
+                {
+                    testAnimator.Play("Walk");
+                }
+                break;
+
+
+            case WalkID.inactive:
+                if (gameObject != null)
+                {
+
+                    testAnimator.Stop();
+                }
+                break;
+
+        }
+
+        return testAnimator;
+    }
+
+
 }
