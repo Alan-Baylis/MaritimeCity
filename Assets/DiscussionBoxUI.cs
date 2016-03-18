0c69b88a00bde49e6060b9f8736a4e5fd13cc31d
Ademola Afolabi <kingsminos@gmail.com>
2016-03-18 15:54:24 +0000

    Update for discussion box ui

--- Assets/DiscussionBoxUI.cs
+++ Assets/DiscussionBoxUI.cs
@@ -57,7 +57,6 @@ public class DiscussionBoxUI : MonoBehaviour {
 	{
 		DiscussionBoxDialog [0] = "What would you be thinking at this point";
 		DiscussionBoxDialog [1] = "Consider the purpose of the visit and what your strategy be for approaching the situation from now on?";
-	
 		//After entering the Lounge and meeting Luke - before the decision point to look at Liam or look at Tara
 		DiscussionBoxDialog [2] = "Now that you have entered the home enviroment how are you feeling?";
 		DiscussionBoxDialog [3] = "How do you intend to engage with the individuals in the home from this point";
@@ -65,7 +64,6 @@ public class DiscussionBoxUI : MonoBehaviour {
 		DiscussionBoxDialog [5] = "What are the warning signs or triggers, if any?";
 		DiscussionBoxDialog [6] = "What protective factors can be identified";
 		DiscussionBoxDialog [7] = "Please describe the interactions (if any) between the children and the adults in the home - do you have any concerns and what are they?";
-
 
 		//On leaving the Home
 		DiscussionBoxDialog [8] = "What would you do now and how would you be feeling?";
